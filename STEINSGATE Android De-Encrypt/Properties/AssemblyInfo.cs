9eea7fa7372c501edfd3a111eb57aea9d6746d82
SKunio <mrboms@users.noreply.github.com>
2018-03-05 13:57:09 +0700

    Add files via upload

---
+++ STEINSGATE Android De-Encrypt/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("STEINSGATE Android De-Encrypt")]
+[assembly: AssemblyDescription("Decrypt & Encrypt STEINSGATE Android (.sdt)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SKunio-VN")]
+[assembly: AssemblyProduct("STEINSGATE Android De-Encrypt")]
+[assembly: AssemblyCopyright("SKunio-VN ©  2017")]
+[assembly: AssemblyTrademark("SKunio-VN")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c380ce3d-c215-4a33-b5b3-29188eb695d4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ STEINSGATE Android De-Encrypt/STEINSGATE Android De-Encrypt.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EncryptDLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--Made By SKunio-VN--");
+            string path = Environment.CurrentDirectory;
+            string[] addFile = Environment.GetCommandLineArgs();
+            Console.WriteLine(path);
+
+            try
+            {
+                if (addFile.Length > 1)
+                {
+                    for (int f = 1; f < addFile.Length; f++)
+                    {
+                        byte[] bytes = File.ReadAllBytes(addFile[f]);
+
+                        string Name = Path.GetFileNameWithoutExtension(addFile[f]);
+                        string FullName = "";
+                        Console.WriteLine("In : " + Path.GetFileName(addFile[f]));
+
+                        Dictionary<string, string> libExt = new Dictionary<string, string>
+                        {
+                           { "SDT", ".sdt" },
+                           { "SRC", ".src" },
+                           { "PNG", ".png" },
+                           { "MP4", ".mp4" }
+                        };
+
+                        if (bytes[0] == 71 && bytes[1] == 8)
+                        {
+                            FullName = Name + libExt["SRC"];
+                            for (int i = 0; i < bytes.Length; i++)
+                            {
+                                bytes[i] ^= 100;
+                            }
+                            goto Next;
+                        }
+                        else if (bytes[0] == 237 && bytes[1] == 52)
+                        {
+                            FullName = Name + libExt["PNG"];
+                        }
+                        else if (bytes[0] == 100 && bytes[1] == 100)
+                        {
+                            FullName = Name + libExt["MP4"];
+                        }
+                        else
+                        {
+                            FullName = Name + libExt["SDT"];
+                            if (Path.GetExtension(addFile[f]) == libExt["SRC"])
+                            {
+                                FullName = Name + libExt["SRC"];
+                                for (int i = 0; i < bytes.Length; i++)
+                                {
+                                    bytes[i] ^= 100;
+                                }
+                                goto Next;
+                            }
+                        }
+
+                        for (int i = 0; i < 100; i++)
+                        {
+                            bytes[i] ^= 100;
+                        }
+
+                    Next:
+                        if (!Directory.Exists(path + "\\OutPut"))
+                        {
+                            Directory.CreateDirectory(path + "\\OutPut");
+                        }
+                        Console.WriteLine("Out : \\OutPut\\" + FullName);
+
+                        File.WriteAllBytes(path + "\\OutPut\\" + FullName, bytes);
+                    }
+                    Console.WriteLine("Press one Key!");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Press one Key!");
+                    Console.WriteLine("File!");
+                    Console.ReadKey();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Press one Key!");
+                Console.WriteLine("Err!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
